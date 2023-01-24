910cb7c918e948dff8d3fa54da8760d794c81658
ThVinicius <vinicioss9955@gmail.com>
2023-01-24 16:32:03 -0300

    feat: new props

--- src/types/productType.ts
+++ src/types/productType.ts
@@ -1,7 +1,14 @@
 export interface IProduct {
   id: number
   name: string
+  description: string
+  category: string
   image: string
   stock: number
   price: number
+  rating: number
+  brand: string
+  hasFreeShipping: boolean
+  minimumQuantity: number
+  kitManagerVariations: string
 }
