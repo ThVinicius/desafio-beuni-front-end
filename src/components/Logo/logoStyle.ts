afb6fa50c3f017f598b1c5b18b9822b663ed342d
ThVinicius <vinicioss9955@gmail.com>
2023-01-25 16:46:43 -0300

    style: logo

---
+++ src/components/Logo/logoStyle.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: fit-content;
+  display: flex;
+  gap: 8px;
+  align-items: baseline;
+
+  h1 {
+    font: normal 700 40px 'Lato', sans-serif;
+  }
+
+  img {
+    height: 38px;
+  }
+`
+
+export { Container }
